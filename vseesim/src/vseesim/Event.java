74b29a3715bb5cd91906ff4cb4d4468ff6b9aad8
tanyaporn.tirapat <tanyaporn.tirapat@50765aee-f201-e6e3-60ca-b84f2f41f132>
2013-07-17 04:31:52 +0000

    dew

---
+++ vseesim/src/vseesim/Event.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vseesim;
+
+import java.util.HashMap;
+
+public class Event implements Comparable<Event>
+{
+    public static final int TYPE_TASK_FIN = 1;
+    public static final int TYPE_FILE_SENT = 2;
+    private HashMap <String,Object> hmap = new HashMap<>();
+    
+    private int time;
+    private int type;
+
+    public Event(int time, int type)
+    {
+        this.time = time;
+        this.type = type;
+    }
+
+    public int getTime()
+    {
+        return time;
+    }
+    
+    public int getType()
+    {
+        return type;
+    }
+
+    public Object getProperty(String s)
+    {
+        return  hmap.get(s);
+    }
+    public void setProperty(String s, Object o)
+    {
+        hmap.put(s,o);
+    }
+    public int compareTo(Event e)
+    {
+        if(this.time < e.time)
+        {
+            return -1;
+        }
+        else if (this.time == e.time)
+        {
+            return 0;
+        }
+        else 
+        {
+            return 1;
+        }
+    }
+    
+    
+}
